e88b2f96afb45e64722a7bda020c577830cb23c6
SantoshThkr <santosh.thakur36911@gmail.com>
2020-03-26 20:30:00 +0530

    add answer selection and navigation

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-function Question({ question }) {
+function Question({ question, selectedAnswer, onSelect }) {
   return (
     <div className="question">
       <h2>{question.question}</h2>
       <div className="options">
         {question.options.map((option) => (
-          <label key={option} className="option">
-            <input type="radio" name="answer" value={option} />
+          <label
+            key={option}
+            className={option === selectedAnswer ? 'option selected' : 'option'}
+          >
+            <input
+              type="radio"
+              name="answer"
+              value={option}
+              checked={option === selectedAnswer}
+              onChange={() => onSelect(option)}
+            />
             {option}
           </label>
         ))}
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,12 +4,41 @@ import Progress from './Progress';
 import Question from './Question';
 
 function Quiz() {
-  const [currentQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [finished, setFinished] = useState(false);
+
+  const isLastQuestion = currentQuestion === questions.length - 1;
+
+  const handleNext = () => {
+    if (!selectedAnswer) {
+      return;
+    }
+
+    if (isLastQuestion) {
+      setFinished(true);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+      setSelectedAnswer('');
+    }
+  };
+
+  if (finished) {
+    return <h2 className="result">Quiz Completed!</h2>;
+  }
 
   return (
     <div>
       <Progress current={currentQuestion + 1} total={questions.length} />
-      <Question question={questions[currentQuestion]} />
+      <Question
+        question={questions[currentQuestion]}
+        selectedAnswer={selectedAnswer}
+        onSelect={setSelectedAnswer}
+      />
+      {!selectedAnswer && <p className="hint">Please select an answer.</p>}
+      <button onClick={handleNext} disabled={!selectedAnswer}>
+        {isLastQuestion ? 'Finish Quiz' : 'Next'}
+      </button>
     </div>
   );
 }
